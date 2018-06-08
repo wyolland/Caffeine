9aee60685581b5b148bba9612099303ad0e5bbf5
Julian <Julian@JULIAN-PC>
2018-06-08 09:46:45 +0200

    structure updated

---
+++ Caffeine/lstm.h
@@ -0,0 +1,45 @@
+#pragma once
+
+
+class LongShortTermMemory
+{
+public:
+	//maybe remove constructor as no object should be createt of this class
+	LongShortTermMemory();
+	~LongShortTermMemory();
+
+	float * input;
+	float * output;
+	float * state;
+private:
+
+};
+
+class DefaultLSTM : LongShortTermMemory
+{
+public:
+	DefaultLSTM();
+	~DefaultLSTM();
+
+};
+
+class GersSchmidhuberLSTM : LongShortTermMemory
+{
+public:
+	GersSchmidhuberLSTM();
+	~GersSchmidhuberLSTM();
+
+private:
+
+};
+
+
+class ChoLSTM : LongShortTermMemory
+{
+public:
+	ChoLSTM();
+	~ChoLSTM();
+
+private:
+
+};
